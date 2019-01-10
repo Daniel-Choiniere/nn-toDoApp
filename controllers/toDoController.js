d7aab7c4226133f466e2a002a7a5e6474defc2a2
Daniel Choiniere <danielchoiniere.student@careerdevs.com>
2019-01-10 23:41:39 +0000

    installed mongoose, created a database on mlab hosting

--- controllers/toDoController.js
+++ controllers/toDoController.js
@@ -1,4 +1,19 @@
 var bodyPasrser = require('body-parser');
+var mongoose = require('mongoose');
+
+// connect to the database 
+mongoose.connect('mongodb://testlist:testlist1@ds153314.mlab.com:53314/todolist');
+
+// Create a schema - this is like a blueprint
+var todoSchema = new mongoose.Schema({
+   item: String 
+});
+
+var Todo = mongoose.model('Todo', todoSchema);
+var itemOne = Todo({item: 'buy flowers'}).save(function(err){
+    if (err) throw err;
+    console.log('item saved');
+})
 
 var data = [{item: 'get milk'}, {item: 'walk dog'}, {item: 'kick some coding ass'}];
 var urlencodedParser = bodyPasrser.urlencoded({extended: false});
