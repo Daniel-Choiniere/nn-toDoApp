9fb916cef9abacd6dfa5059dfc59caf448927d59
Daniel Choiniere <danielchoiniere.student@careerdevs.com>
2019-01-11 00:39:34 +0000

    completed fully functional to do list app with database cloud hosting

--- controllers/toDoController.js
+++ controllers/toDoController.js
@@ -10,28 +10,42 @@ var todoSchema = new mongoose.Schema({
 });
 
 var Todo = mongoose.model('Todo', todoSchema);
-var itemOne = Todo({item: 'buy flowers'}).save(function(err){
-    if (err) throw err;
-    console.log('item saved');
-})
+// var itemOne = Todo({item: 'buy flowers'}).save(function(err){
+//     if (err) throw err;
+//     console.log('item saved');
+// })
 
-var data = [{item: 'get milk'}, {item: 'walk dog'}, {item: 'kick some coding ass'}];
+// var data = [{item: 'get milk'}, {item: 'walk dog'}, {item: 'kick some coding ass'}];
 var urlencodedParser = bodyPasrser.urlencoded({extended: false});
 
 module.exports = function(app){
     
     app.get('/todo', function(req, res){
-        res.render('todo', {todos: data});
+        // get data from mongodb and pass it to view
+        
+        // if i want to retrieve a certain item from the datatbase
+        // Example -- Todo.find({item: 'buy flowers'});
+        
+        // if i want to retrieve all items in database
+        Todo.find({}, function(err, data){
+            if (err) throw err;
+            res.render('todo', {todos: data});
+        });
     });
     
     app.post('/todo', urlencodedParser, function(req, res){
-        data.push(req.body);
-        res.json({todos: data});
+        // get data from the view and add it to mongoDB
+        var newTodo = Todo(req.body).save(function(err, data){
+            if (err) throw err;
+            res.json(data);
+        })
     });
     
     app.delete('/todo/:item', function(req, res){
-        data = data.filter(function(todo){
-           return todo.item.replace(/ /g, '_') !== req.params.item;
+        // delete the requested item from mongoDB
+        Todo.find({item: req.params.item.replace(/\-/g, " ")}).remove(function(err,data){
+            if (err) throw err;
+            res.json(data);
         });
     });
 };
